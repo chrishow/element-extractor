bda94717249aefb1eb71121fb26c7e8af7ee8b72
Chris How <chris@primesolid.com>
2026-02-03 08:52:12 +0000

    Use relative paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/element-extractor/', // GitHub Pages repo name
+    base: './', // Use relative paths for GitHub Pages
     plugins: [
         // ... other plugins if you have any
     ],
